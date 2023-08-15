708433e2c2786562a837bcdeb6feba167bc90e04
tv <tuomas.vahanen@gmail.com>
2023-08-15 23:38:42 +0200

    Image switching and background fading

--- __init__.py
+++ __init__.py
@@ -4,12 +4,32 @@ import st3m.run
 import leds
 import random
 import time
+import math
+
+top_left_x = math.ceil(
+    120 * math.cos(
+        math.radians(360-45)
+    )
+)
+top_left_y = math.ceil(
+    120 * math.sin(
+        math.radians(360-45)
+    )
+)
+
+bottom_right_x = math.ceil(120 * math.cos(180-45))
+bottom_right_y = math.ceil(120 * math.sin(180-45))
 
 class Burn(Application):
     def __init__(self, app_ctx: ApplicationContext) -> None:
         super().__init__(app_ctx)
         random.seed()
         leds.set_slew_rate(10)
+
+
+        self.i = 0
+        self.time = 0
+
         self.fire_array = [
             [128, 17, 0],
             [182, 34, 3],
@@ -18,20 +38,45 @@ class Burn(Application):
             [255, 117, 0],
             [250, 170, 0]
         ]
-        self.i = 0
-        self.time = 0
 
-        self.led_buffer = [[0, 0,0]]*40
-        self.last_draw = 0
-        self.interval = 250
+        self.led_buffer = [self.fire_array[0]]*40
+        self.led_since_step =  0
+        self.led_interval = 250
+        self.led_fire_array_index = 0
+
+
+        self.image_since_step = 0
+        self.image_interval = 10000
+        self.image_index = 1
+        self.images = [
+            [
+                "/flash/sys/apps/cccflower/Exploits.png",
+                -121,
+                -121,
+                242,
+                242
+            ],
+            [
+                "/flash/sys/apps/cccflower/burningflowers.png",
+                -top_left_x,
+                top_left_y,
+
+                math.ceil(math.fabs(top_left_x * 2)),
+                math.ceil(math.fabs(top_left_y * 2)),
+            ]
+        ]
+
+        self.bg_interval = 1400
+        self.bg_color = [0.0, 0.0, 0.0]
+        self.bg_since_step = 0
+        self.bg_steps = [0.0, 0.0, 0.0]
+        self.bg_fire_array_index = 0
         
     def draw(self, ctx: Context) -> None:
+        ctx.rgb(*self.bg_color).rectangle(-120, -120, 240, 240).fill()
+
         ctx.image(
-            "/flash/sys/apps/cccflower/Exploits.png",
-            -121,
-            -121,
-            242,
-            242
+            *self.images[self.image_index]
         )
 
         for i, led in enumerate(self.led_buffer):
@@ -40,29 +85,50 @@ class Burn(Application):
         leds.update()
 
     def think(self, ins: InputState, delta_ms: int) -> None:
-        self.last_draw += delta_ms
+        self.led_since_step += delta_ms
+        self.image_since_step += delta_ms
+        self.bg_since_step += delta_ms
+
+        if self.bg_since_step > self.bg_interval:
+            self.bg_since_step = 0
+            self.bg_fire_array_index = (self.bg_fire_array_index + 1) % len(self.fire_array)
+            chosen_color = self.fire_array[self.bg_fire_array_index]
+            chosen_color = [c / 255.0 for c in chosen_color]
+
+            self.bg_steps = [
+                (chosen_color[i] - self.bg_color[i]) / self.bg_interval
+                for i in range(3)
+            ]
+
+        self.bg_color = [
+            self.bg_color[i] + self.bg_steps[i] * delta_ms
+            for i in range(3)
+        ]
 
         rage = 500 * (delta_ms / 1000)
 
         direction = ins.buttons.app # -1 (left), 1 (right), or 2 (pressed)
-        if direction == -1 and self.interval < 1000:
-            self.interval += rage
+        if direction == -1 and self.led_interval < 1000:
+            self.led_interval += rage
 
-        elif direction == 1 and self.interval > 30:
-            self.interval -= rage
+        elif direction == 1 and self.led_interval > 30:
+            self.led_interval -= rage
 
 
-        if self.last_draw < self.interval:
-            return
+        if self.led_since_step > self.led_interval:
+            self.led_since_step = 0
 
-        self.last_draw = 0
+            indices = list(range(self.led_fire_array_index, len(self.fire_array))) + list(range(0, self.led_fire_array_index))
 
-        for f in self.fire_array:
-            self.led_buffer[self.i] = f
-            self.i = (self.i + 1) % 40
+            for f in indices:
+                self.led_buffer[self.i] = self.fire_array[f]
+                self.i = (self.i + 1) % 40
 
-        c = self.fire_array.pop(0)
-        self.fire_array.append(c)
+            self.led_fire_array_index = (self.led_fire_array_index + 1) % len(self.fire_array)
+
+        if self.image_since_step > self.image_interval:
+            self.image_since_step = 0
+            self.image_index = (self.image_index + 1) % len(self.images)
 
 if __name__ == "__main__":
     st3m.run.run_view(Burn(ApplicationContext()))
