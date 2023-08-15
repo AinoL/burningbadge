179e160c1a9c988ab3f704f5a4deed4eb9b381bb
Onja <onja@Ainos-MacBook-Pro-2.local>
2023-08-15 19:40:49 +0200

    inited led loop

--- __init__.py
+++ __init__.py
@@ -3,38 +3,32 @@ from st3m.reactor import Responder
 import st3m.run
 import leds
 import random
+import time
 
 class Burn(Application):
     def __init__(self, app_ctx: ApplicationContext) -> None:
         super().__init__(app_ctx)
-        leds.set_slew_rate(4)
         random.seed()
-
+        self.fire_array = [
+            [128, 17, 0],
+            [182, 34, 3],
+            [215, 53, 2],
+            [252, 100, 0],
+            [255, 117, 0],
+            [250, 192, 0]
+        ]
+        self.i = 0
+        self.time = 0
+        
     def draw(self, ctx: Context) -> None:
-        # Paint the background black
-        ctx.rgb(0, 0, 0).rectangle(-120, -120, 240, 240).fill()
-
-        ctx.rgb(255, 255, 255)
-        ctx.font = "Camp Font 3"
-
-        text = "burn1ngfl0w3rs"
-        w = ctx.text_width(text)
-        ctx.move_to(-w/2, -20)
-        ctx.text(text)
-
-        text = "tv"
-        w = ctx.text_width(text)
-        ctx.move_to(-w/2, 20)
-        ctx.text(text)
-
-        for i in range(40):
-            leds.set_rgb(
-                i,
-                random.randint(50, 255)/255,
-                random.randint(10, 60)/255,
-                0
-            )
-        leds.update()
+        self.time = (self.time + 1)%10
+        if self.time == 0:
+            for f in self.fire_array:
+                leds.set_rgb(self.i, *f)
+                self.i = (self.i + 1)%40
+            c = self.fire_array.pop(0)
+            self.fire_array.append(c)
+            leds.update()
 
     def think(self, ins: InputState, delta_ms: int) -> None:
         pass
--- burn.py
+++ burn.py
@@ -2,32 +2,29 @@ from st3m.reactor import Responder
 import st3m.run
 from ctx import Context
 
-# import leds
-# import time
+import leds
+import time
 
-# fire_array = [
-#     [128, 17, 0],
-#     [182, 34, 3],
-#     [215, 53, 2],
-#     [252, 100, 0],
-#     [255, 117, 0],
-#     [250, 192, 0]
-# ]
+fire_array = [
+    [128, 17, 0],
+    [182, 34, 3],
+    [215, 53, 2],
+    [252, 100, 0],
+    [255, 117, 0],
+    [250, 192, 0]
+]
 
-# ledcounter = 0
-# offset = 0
-# while True:
-#     while ledcounter < 40:
-#         for i in fire_array:
-#             leds.set_rgb(ledcounter, *i)
-#             leds.update()
-#             time.sleep(0.1)
-#             ledcounter = ledcounter+1
-#     c = fire_array.pop(1)
-#     print(c)
-#     fire_array.append(c)
-#     ledcounter = 0
-#     offset = offset + 1
+ledcounter = 0
+while ledcounter < 40:
+    for i in fire_array:
+        leds.set_rgb(ledcounter, *i)
+        leds.update()
+        time.sleep(0.1)
+        ledcounter = ledcounter+1
+c = fire_array.pop(1)
+print(c)
+fire_array.append(c)
+ledcounter = 0
 
 class Example(Responder):
     def __init__(self) -> None:
