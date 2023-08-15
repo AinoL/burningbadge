721772db0252b1159383fce36dd6abd3d70203bf
Onja <onja@Ainos-MacBook-Pro-2.local>
2023-08-15 18:59:03 +0200

    Image showing

--- burn.py
+++ burn.py
@@ -1,26 +1,48 @@
-import leds
-import time
+from st3m.reactor import Responder
+import st3m.run
+from ctx import Context
 
-fire_array = [
-    [128, 17, 0],
-    [182, 34, 3],
-    [215, 53, 2],
-    [252, 100, 0],
-    [255, 117, 0],
-    [250, 192, 0]
-]
+# import leds
+# import time
 
-ledcounter = 0
-offset = 0
-while True:
-    while ledcounter < 40:
-        for i in fire_array:
-            leds.set_rgb(ledcounter, *i)
-            leds.update()
-            time.sleep(0.1)
-            ledcounter = ledcounter+1
-    c = fire_array.pop(1)
-    print(c)
-    fire_array.append(c)
-    ledcounter = 0
-    offset = offset + 1
+# fire_array = [
+#     [128, 17, 0],
+#     [182, 34, 3],
+#     [215, 53, 2],
+#     [252, 100, 0],
+#     [255, 117, 0],
+#     [250, 192, 0]
+# ]
+
+# ledcounter = 0
+# offset = 0
+# while True:
+#     while ledcounter < 40:
+#         for i in fire_array:
+#             leds.set_rgb(ledcounter, *i)
+#             leds.update()
+#             time.sleep(0.1)
+#             ledcounter = ledcounter+1
+#     c = fire_array.pop(1)
+#     print(c)
+#     fire_array.append(c)
+#     ledcounter = 0
+#     offset = offset + 1
+
+class Example(Responder):
+    def __init__(self) -> None:
+        pass
+
+    def draw(self, ctx: Context) -> None:
+        ctx.image(
+                "/flash/sys/apps/burningbadge/Exploits.png",
+                -121,
+                -121,
+                242,
+                242
+            )
+
+    def think(self, ins: InputState, delta_ms: int) -> None:
+        pass
+
+st3m.run.run_responder(Example())
